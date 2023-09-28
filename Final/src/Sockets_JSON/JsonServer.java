1794f018cd10cd6efbb8d7c44bea611dd9a44708
JBB092 <jos.barquero@estudiantec.cr>
2023-09-28 00:27:06 -0600

    Conexión Sockets mejorada
    Ahora se pueden conectar una "n" cantidad de clientes al servidor.

--- Final/src/Sockets_JSON/JsonServer.java
+++ Final/src/Sockets_JSON/JsonServer.java
@@ -1,5 +1,4 @@
 package Sockets_JSON;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.BufferedReader;
@@ -17,34 +16,53 @@ public class JsonServer {
             ServerSocket serverSocket = new ServerSocket(12345);
             System.out.println("Servidor iniciado. Esperando conexiones...");
 
-            // Esperar a que un cliente se conecte
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
+            while (true) {
+                // Esperar a que un cliente se conecte
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
 
-            // Configurar streams de entrada y salida
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-            // Recibir el mensaje en formato JSON desde el cliente
-            String jsonMessage = in.readLine();
-
-            // Convertir el JSON a un objeto
-            ObjectMapper objectMapper = new ObjectMapper();
-            Message message = objectMapper.readValue(jsonMessage, Message.class);
-
-            System.out.println("Mensaje recibido del cliente: " + message);
-
-            // Enviar una respuesta al cliente
-            Message responseMessage = new Message("Servidor", "Respuesta recibida");
-            String jsonResponse = objectMapper.writeValueAsString(responseMessage);
-            out.println(jsonResponse);
-
-            // Cerrar conexiones
-            clientSocket.close();
-            serverSocket.close();
+                // Crear un hilo para manejar la comunicación con este cliente
+                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                clientThread.start();
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-}
 
+    private static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Configurar streams de entrada y salida
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                // Recibir el mensaje en formato JSON desde el cliente
+                String jsonMessage = in.readLine();
+
+                // Convertir el JSON a un objeto
+                ObjectMapper objectMapper = new ObjectMapper();
+                Message message = objectMapper.readValue(jsonMessage, Message.class);
+
+                System.out.println("Mensaje recibido del cliente: " + message);
+
+                // Enviar una respuesta al cliente
+                Message responseMessage = new Message("Servidor", "Respuesta recibida");
+                String jsonResponse = objectMapper.writeValueAsString(responseMessage);
+                out.println(jsonResponse);
+
+                // Cerrar conexión con este cliente
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
